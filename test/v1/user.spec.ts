2b3737e297bd7836ccbfc4cb9b1903f22ec6c9a7
pradip <pradip.kharal777@gmail.com>
2024-03-31 21:09:32 +0545

    initialize test

---
+++ test/v1/user.spec.ts
@@ -0,0 +1,9 @@
+import { it } from 'mocha'
+import { UserController } from '../../api/v1/controllers'
+
+describe('Test Get user', () => {
+   it('Should get one user by id', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const userController = new UserController()
+   })
+})
